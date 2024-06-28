7957089af3b9505033b2ea26a6ae35e9ca0075a8
hvtheer <hthe1010@gmail.com>
2024-06-28 17:48:09 +0700

    feat: Refactor job repository to include company details in job retrieval

--- app/repositories/job.py
+++ app/repositories/job.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
-
-from app.models import Job
+from app.models import *
+from app.models.company import Company
 from .base import BaseRepository
 
 
@@ -13,6 +13,26 @@ class JobRepository(BaseRepository[Job]):
             column_id=Job.job_id,
         )
 
-    # def get_jobs_active(self, pagination, condition, order_by):
-    #     # condition = and_(Job.status > 0, condition)
-    #     self.get_all(pagination, condition, order_by)
+    def get_jobs_with_company(self, pagination, condition, order_by):
+        # Create a JOIN between Job, Company, and JobSkill tables
+        query = self.session.query(Job, Company).join(
+            Company, Job.company_id == Company.company_id
+        )
+
+        # Apply condition, order_by, and pagination
+        if condition is not None:
+            query = query.filter(condition)
+        if order_by is not None:
+            query = query.order_by(order_by)
+        if pagination:
+            if "page" in pagination and "size" in pagination:
+                page = pagination["page"]
+                size = pagination["size"]
+                query = query.offset((page - 1) * size).limit(size)
+
+        # Fetch all records and return as a list of Job objects with company details and job skills
+        jobs_with_company = []
+        for job, company in query.all():
+            job.company = company
+            jobs_with_company.append(job)
+        return jobs_with_company
